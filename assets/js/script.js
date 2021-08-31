adc6ecef12f288e5d8402d8740364299b2861adf
unknown <jasdeepsaini30@gmail.com>
2021-08-31 15:03:05 -0400

    adding updates on js

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,10 +12,12 @@ var feedbackEl = document.getElementById("feedback");
 var endSection = document.getElementById("end");
 var endTitle = document.getElementById("end-title");
 var SCORE = document.getElementById("score");
-var INITIALS_INPUT = document.getElementById("initials");
+var INITIALS_INPUT = document.getElementById("input-initials");
 var SUBMIT_SCORE = document.getElementById("submit-score");
 var ERROR_MESSAGE = document.getElementById("error-message");
 
+var submitButton = document.getElementById("submit-btn");
+var scoreList = document.getElementById("score-list")
 
 var timeLeft = 0
 var quizTime = 0
@@ -198,78 +200,10 @@ function loadNextQuestion() {
     }
 
     // // submit initials 
-    // function processInput(event) {
-    //     event.preventDefault();
 
-    //     var initials = INITIALS_INPUT.value.toUpperCase();
 
-    //     if (isInputValid(initials)) {
-    //         var score = count;
-    //         var highscoreEntry = getNewHighscoreEntry(initials, score);
-    //         saveHighscoreEntry(highscoreEntry);
-    //         window.location.href = "./highscore.html";
-    //     }
-    // }
+    //add intials to local storage
 
-    // function getNewHighscoreEntry(initials, score) {
-    //     var entry = {
-    //         initials: initials,
-    //         score: score,
-    //     }
-    //     return entry;
-    // }
-
-    // function isInputValid(initials) {
-    //     let errorMessage = "";
-    //     if (initials === "") {
-    //         errorMessage = "You can't submit empty initials!";
-    //         displayFormError(errorMessage);
-    //         return false;
-    //     } else if (initials.match(/[^a-z]/ig)) {
-    //         errorMessage = "Initials may only include letters."
-    //         displayFormError(errorMessage);
-    //         return false;
-    //     } else {
-    //         return true;
-    //     }
-    // }
-
-    // function displayFormError(errorMessage) {
-    //     ERROR_MESSAGE.textContent = errorMessage;
-    //     if (!INITIALS_INPUT.classList.contains("error")) {
-    //         INITIALS_INPUT.classList.add("error");
-    //     }
-    // }
-
-    // function saveHighscoreEntry(highscoreEntry) {
-    //     var currentScores = getScoreList();
-    //     placeEntryInHighscoreList(highscoreEntry, currentScores);
-    //     localStorage.setItem('scoreList', JSON.stringify(currentScores));
-    // }
-
-    // function getScoreList() {
-    //     const currentScores = localStorage.getItem('scoreList');
-    //     if (currentScores) {
-    //         return JSON.parse(currentScores);
-    //     } else {
-    //         return [];
-    //     }
-    // }
-
-    // function placeEntryInHighscoreList(newEntry, scoreList) {
-    //     const newScoreIndex = getNewScoreIndex(newEntry, scoreList);
-    //     scoreList.splice(newScoreIndex, 0, newEntry);
-    // }
-
-    // function getNewScoreIndex(newEntry, scoreList) {
-    //     if (scoreList.length > 0) {
-    //         for (let i = 0; i < scoreList.length; i++) {
-    //             if (scoreList[i].score <= newEntry.score) {
-    //                 return i;
-    //             }
-    //         }
-    //     }
-    //     return scoreList.length;
-    // }
 }
+
 startbutton.addEventListener("click", startquiz);
